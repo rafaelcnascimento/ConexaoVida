40c86944ebda720cebec5c781524158f04210a8c
Rafael <rafaelcatelandonascimento@gmail.com>
2018-11-07 23:03:18 +0000

    Erro 404, adicionado imagens

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <title>{{ config('app.name', 'Laravel') }}</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">  
+        {{-- Fonte --}}
+        <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    </head>
+    <style type="text/css">
+        .texto{
+            font-family: 'Montserrat', sans-serif;
+            font-size: 30px;
+        }
+    </style>
+    <body>
+        <center>
+            <div class="texto" style="margin-top: 15%">
+                Essa página não existe
+            </div>    
+        </center>
+       
+    </body>
+</html>
